bd3bfb1f2b26558e1b0ec43d84804186891395e4
jasminenoack <jasminenoack@gmail.com>
2015-01-07 22:24:57 -0500

    show index

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -5,9 +5,12 @@ class PuzzlesController < ApplicationController
     params.each do |item|
       squares<<item if item[0].count(",")>0
     end
-    squares
+    squares.map{|x| x[1].to_i}
   end
 
+  def index
+    @puzzles = Puzzle.all
+  end
 
   def new
     @puzzle = Puzzle.new
@@ -18,6 +21,18 @@ class PuzzlesController < ApplicationController
     @squares=parse_params
     @puzzle.board=@puzzle.setup_board(@squares)
     @puzzle.save
+    redirect_to puzzle_path(@puzzle.id)
   end
 
+  def show
+    @puzzle=Puzzle.find(params[:id])
+  end
+
+
+
 end
+
+
+
+# puzzle board params come in this order if numbered 1-81 left to right top to bottom: 
+# [1, 2, 3, 10, 11, 12, 19, 20, 21, 4, 5, 6, 13, 14, 15, 22, 23, 24, 7, 8, 9, 16, 17, 18, 25, 26, 27, 28, 29, 30, 37, 38, 39, 46, 47, 48, 31, 32, 33, 40, 41, 42, 49, 50, 51, 34, 35, 36, 43, 44, 45, 52, 53, 54, 55, 56, 57, 64, 65, 66, 73, 74, 75, 58, 59, 60, 67, 68, 69, 76, 77, 78, 61, 62, 63, 70, 71, 72, 79, 80, 81]
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,16 +1,35 @@
 class Puzzle < ActiveRecord::Base
-   serialize :board
+  serialize :board
+
+  #switches between display array and working array
+  def switch_board(array = board)
+    result=[]
+    rows=Array.new
+    #selects all rows within a row of squares on the grid
+    array.each_slice(27) do |row_set|
+      rows=[[],[],[]]
+      #selects a square
+      row_set.each_slice(9) do |square|
+        square.each_with_index do |element, index|
+          case index
+          when (0..2) then rows[0] << element
+          when (3..5) then rows[1] << element
+          when (6..8) then rows[2] << element
+          end
+        end
+      end
+      result << rows.flatten
+    end
+    result.flatten
+  end
 
   def setup_board(parameters)
-    @board=[]
-    parameters.each do |param|
-        unless param[1]==""
-          @board<<param[1].to_i
-        else
-          @board<<nil
-        end
-    end
-    @board
+    @board=switch_board(parameters)
   end
+
+  def display_board
+    switch_board
+  end
+
 
 end
