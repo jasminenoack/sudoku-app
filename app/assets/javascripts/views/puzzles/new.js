c3d1427f8e9983fb96dee32f737a6e74cff40d9c
jasminenoack <jasminenoack@gmail.com>
2015-03-22 01:45:07 -0400

    creates a working form in backbone

--- app/assets/javascripts/views/puzzles/new.js
+++ app/assets/javascripts/views/puzzles/new.js
@@ -4,9 +4,35 @@ Sudoku.Views.New = Backbone.View.extend({
 
   template: JST["sudokuForm"],
 
+  events: {
+    "submit .puzzle":"newPuzzle",
+  },
+
   render: function () {
     this.$el.html(this.template())
     return this
   },
 
+  newPuzzle: function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    this.board = this.serializePuzzleForm($(event.currentTarget))
+    console.log("board", board)
+  },
+
+  serializePuzzleForm: function ($target) {
+    board = []
+    _($target.children()).each(function (row) {
+      board = board.concat(this.readRow($(row)))
+    }.bind(this))
+    return board
+  },
+
+  readRow: function ($row) {
+    rowValues = []
+    _($row.children()).each(function (square) {
+      rowValues.push($(square).children()[0].value || 0)
+    })
+    return rowValues
+  },
 })
